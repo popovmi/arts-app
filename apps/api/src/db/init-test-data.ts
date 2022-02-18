d50cef6168f19513edca7c8c1f98ff8f640e43bb
aenkas <mi69popov@gmail.com>
2022-02-18 13:07:38 +0300

    init full test data

--- apps/api/src/db/init-test-data.ts
+++ apps/api/src/db/init-test-data.ts
@@ -1,15 +1,22 @@
-import { hash } from 'bcrypt';
 import { Art } from '@/modules/art/entity/art.entity';
+import * as AttrEntities from '@/modules/attribute/entities';
+import { Customer } from '@/modules/customer/entities/customer.entity';
+import { Factory } from '@/modules/factory/entities/factory.entity';
 import { Project } from '@/modules/project/entity/project.entity';
 import { User } from '@/modules/user/entity/user.entity';
 import { Role } from '@/modules/user/role.enum';
+import { hash } from 'bcrypt';
 import { EntityManager } from 'typeorm';
 
-import * as AttrEntities from '@/modules/attribute/entities';
-
 export const initTestData = async (em: EntityManager) => {
-    const userRep = em.getRepository(User);
     const users = [];
+    const attributes = [];
+    let customers = [];
+    let factories = [];
+    const projects = [];
+    const arts = [];
+    const attributesValues = ['qq', 'ww', 'ee', 'rr', 'tt', 'yy'];
+
     users.push({
         username: `ADMIN`,
         role: Role.ADMIN,
@@ -26,33 +33,11 @@ export const initTestData = async (em: EntityManager) => {
             password: await hash('USER', 10),
         });
     }
-    await userRep.save(users);
+    await em.save(User, users);
 
-    const projectRep = em.getRepository(Project);
-    let projects = [];
-    for (let i = 0; i < 200; i++) {
-        projects.push({
-            name: `PROJECT-${i}`,
-            internal: Math.random() < 0.5,
-            hasDesignDoc: Math.random() < 0.5,
-        });
-    }
-    projects = await projectRep.save(projects);
-
-    const artsRep = em.getRepository(Art);
-    let arts = [];
-    for (let i = 0; i < 600; i++) {
-        arts.push({
-            name: `ART-${i}`,
-            internal: Math.random() < 0.5,
-            projectId: projects[i > 200 ? (i > 400 ? i - 400 : i - 200) : i]?.id,
-        });
-    }
-    arts = await artsRep.save(arts);
-
-    let attributes = [];
     for (const AttrEntity of Object.values(AttrEntities)) {
         let i = 1;
+
         attributes.push(em.create(AttrEntity, { name: 'qq', valueOrder: i++ }));
         attributes.push(em.create(AttrEntity, { name: 'ww', valueOrder: i++ }));
         attributes.push(em.create(AttrEntity, { name: 'ee', valueOrder: i++ }));
@@ -60,5 +45,51 @@ export const initTestData = async (em: EntityManager) => {
         attributes.push(em.create(AttrEntity, { name: 'tt', valueOrder: i++ }));
         attributes.push(em.create(AttrEntity, { name: 'yy', valueOrder: i++ }));
     }
-    attributes = await em.save(attributes);
+    await em.save(attributes);
+
+    for (let i = 0; i < 20; i++) {
+        customers.push({ name: `Customer ${i}` });
+    }
+    customers = await em.save(Customer, customers);
+
+    for (let i = 0; i < 20; i++) {
+        factories.push({ name: `Factory ${i}` });
+    }
+    factories = await em.save(Factory, factories);
+
+    for (let i = 0; i < 200; i++) {
+        projects.push({
+            name: `PROJECT-${i}`,
+            internal: Math.random() < 0.5,
+            hasDesignDoc: Math.random() < 0.5,
+            sfm: attributesValues[Math.floor(random(1, 6)) - 1],
+            intercenter: attributesValues[Math.floor(random(1, 6)) - 1],
+            dropNumber: attributesValues[Math.floor(random(1, 6)) - 1],
+            factoryId: factories[Math.floor(random(1, factories.length)) - 1].id,
+            customerId: customers[Math.floor(random(1, customers.length)) - 1].id,
+        });
+    }
+    await em.save(Project, projects);
+
+    for (let i = 0; i < 600; i++) {
+        arts.push({
+            name: `ART-${i}`,
+            internal: Math.random() < 0.5,
+            projectId: projects[i >= 200 ? (i >= 400 ? i - 400 : i - 200) : i]?.id,
+
+            artClass: attributesValues[Math.floor(random(1, 6)) - 1],
+            bottomForm: attributesValues[Math.floor(random(1, 6)) - 1],
+            form: attributesValues[Math.floor(random(1, 6)) - 1],
+            height: attributesValues[Math.floor(random(1, 6)) - 1],
+            nominalVolume: attributesValues[Math.floor(random(1, 6)) - 1],
+            productionMethod: attributesValues[Math.floor(random(1, 6)) - 1],
+            productType: attributesValues[Math.floor(random(1, 6)) - 1],
+            ringType: attributesValues[Math.floor(random(1, 6)) - 1],
+        });
+    }
+    await em.save(Art, arts);
 };
+
+function random(min, max) {
+    return min + Math.random() * (max - min);
+}
